cd0ca8a6a6f11eaffda0d8e07a1e8ba78552f8d7
BARAKA <itsbarakake@gmail.com>
2025-07-13 15:29:38 +0300

    Create App.jsx

---
+++ client/src/services/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the MERN Blog App 📝</h1>
+      <p>Start building your posts and categories!</p>
+    </div>
+  );
+}
+
+export default App;
